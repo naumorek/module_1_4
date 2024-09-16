7684fae8d5d42a07566e9de3d0f3fae39dc9077b
naumorek <38781843+naumorek@users.noreply.github.com>
2024-09-16 13:13:16 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string="Мы можем помолчать, Мы можем петь, Стоять или бежать, Но все равно гореть"
+print(my_string)
+lenth=len(my_string)
+print("длина строки "+ str(lenth))
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.replace(" ", ""))
+print(my_string[lenth-1])
